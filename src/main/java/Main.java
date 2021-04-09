781dea7b6c7625279bef44ae24ec0252f4967a58
MassPixel <64043878+MassPixel@users.noreply.github.com>
2021-04-09 15:16:24 +0100

    added buttons in fxml and loaded fxml in Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,12 @@
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.net.URL;
 
 
 public class Main extends Application {//parent class, abstract methods
@@ -23,24 +19,11 @@ public class Main extends Application {//parent class, abstract methods
 
     @Override
     public void start(Stage stage) throws Exception { //create UI
-        stage.setTitle("Hello, world!");
-        //stage.setHeight(500);
-        //stage.setWidth(300);
-        StackPane root = new StackPane(); //simple, non resizeable layout
-        Scene scene = new Scene(root, 300, 400); //layout
-        scene.setFill(Color.MAROON);
-        //Image icon = new Image("D2BHG8ZU0AAZuPC.jpg");
-        //stage.getIcons().add(icon);
-        Button btn = new Button("Click me"); //leaf node off root
-        btn.setPrefSize(69, 69);
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                System.out.println("Hello, world!");
-            }
-        });
-        root.getChildren().add(btn);//add widget to scene, via root
+        URL url = new File("src/main/java/Toolkit.fxml").toURI().toURL();
+        Parent root = FXMLLoader.load(url);
+        Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+
     }
 }
